b776db5d9a378f8e4beced49bbca1830f71190e4
feyzieskar <feyziekar@gmail.com>
2025-01-13 19:39:53 +0300

    root

--- routers/auth.py
+++ routers/auth.py
@@ -1,9 +1,65 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends, HTTPException
+from pydantic import BaseModel
+from passlib.context import CryptContext
+from sqlalchemy.orm import Session
+from typing import Annotated
+from fastapi.security import OAuth2PasswordRequestForm, OAuth2PasswordBearer
+from starlette import status
+
+from database import SessionLocal
+from models import User
 
 router = APIRouter(
     prefix="/auth",
     tags=["Authentications"],)
 
-@router.get("/get_user")
-async def get_user():
-    return "Hello World!"
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+db_dependency = Annotated[Session, Depends(get_db)]
+
+bcrypt_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+class CreateUserRequest(BaseModel):
+    username: str
+    email: str
+    password: str
+    role: str
+    first_name: str
+    last_name: str
+
+def authenticate_user(username: str, password: str, db):
+    user = db.query(User).filter(User.username == username).first()
+    if not user:
+        return False
+    if not bcrypt_context.verify(password, user.hashed_password):
+        return False
+    return user
+
+
+@router.post("/", status_code=status.HTTP_201_CREATED)
+async def create_user(db: db_dependency, create_user_request: CreateUserRequest):
+    user = User(
+        username=create_user_request.username,
+        email=create_user_request.email,
+        role=create_user_request.role,
+        first_name=create_user_request.first_name,
+        last_name=create_user_request.last_name,
+        is_active=True,
+        hashed_password=bcrypt_context.hash(create_user_request.password),
+    )
+    db.add(user)
+    db.commit()
+
+@router.post("/token",)
+async def login_for_access_token(form_data: Annotated[OAuth2PasswordRequestForm,Depends()],
+                                 db: db_dependency):
+    user = authenticate_user(form_data.username, form_data.password, db)
+    if not user:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Incorrect username or password")
+    token = ""
+    return {"access_token": token, "token_type": "bearer"}
