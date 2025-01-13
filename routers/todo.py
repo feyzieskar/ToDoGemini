0b204eb8b392a69ba183dbadb34ae08b50010891
feyzieskar <feyziekar@gmail.com>
2025-01-13 23:35:21 +0300

    Filter - Limit

--- routers/todo.py
+++ routers/todo.py
@@ -8,6 +8,7 @@ from models import Base, Todo
 from database import engine, SessionLocal
 from typing import Annotated
 from routers.auth import router as auth_router
+from routers.auth import get_current_user
 
 router = APIRouter(
 prefix="/todo",
@@ -28,10 +29,13 @@ def get_db():
         db.close()
 
 db_dependency = Annotated[Session, Depends(get_db)]
+user_dependency = Annotated[dict, Depends(get_current_user)]
 
-@router.get("/read_all")
-async def read_all(db: db_dependency):
-    return db.query(Todo).all()
+@router.get("/")
+async def read_all(user: user_dependency, db: db_dependency):
+    if user is None:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Unauthorized")
+    return db.query(Todo).filter(Todo.owner_id == user.get('id')).all()
 
 @router.get("/get_by_id/{todo_id}",status_code=status.HTTP_200_OK)
 async def read_by_id(db: db_dependency, todo_id: int = Path(gt=0)):
@@ -41,8 +45,11 @@ async def read_by_id(db: db_dependency, todo_id: int = Path(gt=0)):
     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Todo not found")
 
 @router.post("/create_todo",status_code=status.HTTP_201_CREATED)
-async def create_todo(db: db_dependency, todo_request: TodoRequest):
-    todo = Todo(**todo_request.dict())
+async def create_todo(user:user_dependency, db: db_dependency, todo_request: TodoRequest):
+    if user is None:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Unauthorized")
+
+    todo = Todo(**todo_request.dict(), owner_id=user.get('id'))
     db.add(todo)
     db.commit()
 
